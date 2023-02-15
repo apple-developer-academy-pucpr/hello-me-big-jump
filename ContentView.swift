b536538a420c61b84dfc0ccf737b6dfadb61cc44
Djenifer R. P <djeniferrenata@yahoo.com.br>
2023-02-15 16:19:49 -0300

    Add pause and resume buttons

--- ContentView.swift
+++ ContentView.swift
@@ -3,20 +3,47 @@ import SpriteKit
 
 struct ContentView: View {
     @StateObject var gameManager = GameManager()
-    
-    var scene: SKScene {
-        let scene = GameScene()
-        scene.manager = gameManager
-        scene.scaleMode = .resizeFill
-        return scene
-    }
-    
+
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            SpriteView(scene: scene, debugOptions: [.showsFPS, .showsNodeCount])
+        ZStack(alignment: .top) {
+            SpriteView(scene: gameManager.scene, debugOptions: [.showsFPS, .showsNodeCount])
                 .ignoresSafeArea()
 
+            if gameManager.gamePaused {
+                Text("Paused")
+                    .font(.system(.largeTitle, design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundColor(.black)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.white.opacity(0.3))
+            }
+
+            controls
+        }
+    }
+
+    var controls: some View {
+        HStack {
+            if gameManager.gamePaused {
+                createButton(imageName: "play.fill", action: gameManager.resumeGame)
+            } else {
+                createButton(imageName: "pause.fill", action: gameManager.pauseGame)
+            }
+
+            Spacer()
+
             Text("Score: \(gameManager.score)")
+                .foregroundColor(.white)
+                .padding()
+                .background(.black)
+                .cornerRadius(16)
+                .padding()
+        }
+    }
+
+    @ViewBuilder func createButton(imageName: String, action: @escaping () -> Void) -> some View {
+        Button(action: action) {
+            Image(systemName: imageName)
                 .foregroundColor(.white)
                 .padding()
                 .background(.black)
--- GameManager.swift
+++ GameManager.swift
@@ -2,11 +2,29 @@ import Foundation
 
 class GameManager: ObservableObject, GameManagerDelegate {
     @Published var score = 0
+    @Published var gamePaused = false
+
+    lazy var scene: GameScene = {
+        let s = GameScene()
+        s.scaleMode = .resizeFill
+        s.manager = self
+        return s
+    }()
 
     func newHighestPosition(_ position: CGPoint, interval: CGFloat) {
         let value = Int(abs(position.y) / interval)
         if value > score {
             score = value
         }
+    }
+
+    func pauseGame() {
+        scene.isPaused = true
+        gamePaused = true
+    }
+
+    func resumeGame() {
+        scene.isPaused = false
+        gamePaused = false
     }
 }
