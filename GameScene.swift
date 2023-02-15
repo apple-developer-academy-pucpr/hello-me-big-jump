38ad35be9e41fa9f2677a08c4483d5593302e9ed
Djenifer R. P <djeniferrenata@yahoo.com.br>
2023-02-15 11:34:03 -0300

    Add physics and boundaries

--- GameScene.swift
+++ GameScene.swift
@@ -1,8 +1,14 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var boundary: SKNode!
+
     var lightBall: SKSpriteNode!
     var ground: SKSpriteNode!
+
+    let lightBallCategory: UInt32 = 1 // 2^0
+    let platformCategory: UInt32  = 2 // 2^1
+    let boundaryCategory: UInt32  = 4 // 2^2
 
     override func didMove(to view: SKView) {
         backgroundColor = UIColor(red: 109/255, green: 83/255, blue: 143/255, alpha: 1)
@@ -11,6 +17,20 @@ class GameScene: SKScene {
         configureLightBall()
         configureGround()
         configurePlatforms()
+
+        configureBoundaries()
+    }
+
+    func configureBoundaries() {
+        boundary = SKNode()
+        boundary.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        boundary.physicsBody?.categoryBitMask = boundaryCategory
+        boundary.physicsBody?.collisionBitMask = lightBallCategory
+        boundary.position = .zero
+
+        addChild(boundary)
+
+        lightBall.physicsBody?.applyForce(CGVector(dx: 200, dy: 200))
     }
 
     func configureLightBall() {
@@ -19,6 +39,13 @@ class GameScene: SKScene {
         lightBall.position = CGPoint(x: frame.midX - lightBall.frame.midX, y: frame.midY - lightBall.frame.midX)
         lightBall.zPosition = 2
 
+        lightBall.physicsBody = SKPhysicsBody(circleOfRadius: lightBall.size.width / 2)
+        lightBall.physicsBody?.affectedByGravity = true
+        lightBall.physicsBody?.isDynamic = true
+        lightBall.physicsBody?.restitution = 1
+        lightBall.physicsBody?.categoryBitMask = lightBallCategory
+        lightBall.physicsBody?.collisionBitMask = platformCategory | boundaryCategory
+
         addChild(lightBall)
     }
 
@@ -26,6 +53,11 @@ class GameScene: SKScene {
         ground = SKSpriteNode(imageNamed: "neverendingbar")
         ground.position = CGPoint(x: frame.midX, y: frame.minY + ground.frame.height / 2)
         ground.zPosition = 1
+
+        ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+        ground.physicsBody?.affectedByGravity = false
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.categoryBitMask = platformCategory
 
         addChild(ground)
     }
@@ -41,6 +73,11 @@ class GameScene: SKScene {
             let platform = SKSpriteNode(imageNamed: "simplebar")
             platform.position = CGPoint(x: posX, y: posY)
             platform.zPosition = 1
+
+            platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
+            platform.physicsBody?.affectedByGravity = false
+            platform.physicsBody?.isDynamic = false
+            platform.physicsBody?.categoryBitMask = platformCategory
 
             addChild(platform)
 
